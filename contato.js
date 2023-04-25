38d0eaac39b5e4c47893cf1556b56fdb1bd96826
luis <luisguibarreto8@gmail.com>
2023-04-25 19:49:07 -0300

    Adicionando validação no formulário

--- contato.js
+++ contato.js
@@ -1,3 +1,10 @@
 function cadastrar(){
+        let name = document.getElementById("name").value
+        let number = document.getElementById("number").value
+        let email = document.getElementById("email").value
     
+        if (name === "" || number === "" || email === ""){
+            alert("Preencha todos os campos.");
+            return false;
+        }
 }
