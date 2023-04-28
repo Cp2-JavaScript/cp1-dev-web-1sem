a451f6c829475418f7e934d614cb8200ba18684c
luis <luisguibarreto8@gmail.com>
2023-04-28 18:26:01 -0300

    finalizando o projeto

--- quiz.js
+++ quiz.js
@@ -1,40 +1,57 @@
-//alert("Vamos conhecer sobre vinho?")
+alert("Vamos conhecer sobre Vinho!")
+let pontos = 0;
 
-const options = document.querySelectorAll('.option');
+// perguntas
+let resposta1 = prompt("Qual é a principal uva utilizada na produção de vinho tinto na região de Bordeaux?");
+if(resposta1.toLowerCase() === "cabernet sauvignon") {
+    pontos++;
+}
 
-options.forEach(option => {
-    option.addEventListener('click', () => {
-        if (option.textContent === 'Brasília') {
-            alert('Resposta correta!');
-        } else {
-            alert('Resposta incorreta!');
-        }
-    });
-});
+let resposta2 = prompt("Qual é a uva utilizada na produção do vinho do porto?");
+if(resposta2.toLowerCase() === "touriga nacional") {
+    pontos++;
+}
 
-const quizzes = document.querySelectorAll('.quiz');
+let resposta3 = prompt("Qual é a principal uva utilizada na produção do vinho Chianti?");
+if(resposta3.toLowerCase() === "sangiovese") {
+    pontos++;
+}
 
-submitButton.addEventListener('click', () => {
-    let score = 0;
-    
-    quizzes.forEach((quiz, index) => {
-        const question = quiz.querySelector('.question');
-        const options = quiz.querySelectorAll('.option');
-        
-        options.forEach(option => {
-            if (option.textContent === answers[index]) {
-                option.style.backgroundColor = 'green';
-            } else if (option.classList.contains('selected')) {
-                option.style.backgroundColor = 'red';
-            }
-            option.removeEventListener('click', selectOption);
-            option.style.cursor = 'default';
-        });
-        
-        const selectedOption = quiz.querySelector('.selected');
-        
-        if (selectedOption && selectedOption.textContent === answers[index]) {
-            score++;
-        }
-    });
-});
+let resposta4 = prompt("Qual é a uva utilizada na produção do vinho branco Riesling?");
+if(resposta4.toLowerCase() === "riesling") {
+    pontos++;
+}
+
+let resposta5 = prompt("Qual é a uva utilizada na produção do vinho branco Chardonnay?");
+if(resposta5.toLowerCase() === "chardonnay") {
+    pontos++;
+}
+
+let resposta6 = prompt("Qual é o nome do vinho tinto produzido na região de Bordeaux?");
+if(resposta6.toLowerCase() === "bordeaux") {
+    pontos++;
+}
+
+let resposta7 = prompt("Qual é o tipo de vinho produzido na região da Toscana, na Itália?");
+if(resposta7.toLowerCase() === "chianti") {
+    pontos++;
+}
+
+let resposta8 = prompt("Qual é a uva utilizada na produção do vinho branco Sauvignon Blanc?");
+if(resposta8.toLowerCase() === "sauvignon blanc") {
+    pontos++;
+}
+
+let resposta9 = prompt("Qual é a uva utilizada na produção do vinho tinto Syrah?");
+if(resposta9.toLowerCase() === "syrah") {
+    pontos++;
+}
+
+let resposta10 = prompt("Qual é o país que produz o vinho Sauvignon Blanc?");
+if(resposta10.toLowerCase() === "nova zelândia") {
+    pontos++;
+}
+
+// exibe o resultado
+let resultado = document.getElementById("resultado");
+resultado.innerHTML = `Você acertou ${pontos} de 10 perguntas sobre vinho.`
